4c74285648ce9025f9657fbae43299ea08fdc0a5
MariamZead <mariam.a.zead@gmail.com>
2023-12-22 00:53:11 +0200

    Manipulating the bookmark list

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let siteTitleInput = document.querySelector("#siteTitle");
 let siteUrlInput = document.querySelector("#siteUrl");
 let siteDescriptionInput = document.querySelector("#siteDescription");
 const addSiteBtn = document.querySelector("#addSite");
-const udpateSiteBtn = document.querySelector("#updateSite");
+const updateSiteBtn = document.querySelector("#updateSite");
 let currentSiteIndex; // used for the Update button
 
 // ? Alert Modal Variables
@@ -52,17 +52,21 @@ displayBookmarks();
 // ? Showing the bookmarks to the user
 function displayBookmarks() {
   let bookmarksStr = "";
-  for (let i = 0; i < bookmarksList.length; i++) {
+  for (
+    let bookMarkIndex = 0;
+    bookMarkIndex < bookmarksList.length;
+    bookMarkIndex++
+  ) {
     bookmarksStr += `<div class="col-12">
           <div class="bookmark-site p-3 mx-auto d-flex align-items-center justify-content-between gap-4">
             <div class="site-info">
-              <h4 class="site-title">${bookmarksList[i].siteTitle}</h4>
-              <p class="bookmark-desc">${bookmarksList[i].siteDesc}</p>
+              <h4 class="site-title">${bookmarksList[bookMarkIndex].siteTitle}</h4>
+              <p class="bookmark-desc">${bookmarksList[bookMarkIndex].siteDesc}</p>
             </div>
             <div class="site-action d-flex align-items-center gap-3">
-              <a><i class="fa-solid fa-link"></i></a>
-              <i class="fa-solid fa-pen"></i>
-              <i class="fa-solid fa-trash"></i>
+              <a onclick="visitSiteBtn(${bookMarkIndex})"><i class="fa-solid fa-link"></i></a>
+              <i class="fa-solid fa-pen" onclick="editSiteBtn(${bookMarkIndex})"></i>
+              <i class="fa-solid fa-trash" onclick="deleteSiteBtn(${bookMarkIndex})"></i>
             </div>
           </div>
         </div>`;
@@ -89,5 +93,45 @@ addSiteBtn.addEventListener("click", function () {
   bookmarksList.push(bookmark);
   displayBookmarks();
   clearInputs();
-  localStorage.setItem("siteList", JSON.stringify(bookmarksList));
+  localStorage.setItem("bookmarksList", JSON.stringify(bookmarksList));
+});
+
+// ? Accessing a website using its URL.
+function visitSiteBtn(siteIndex) {
+  let sitePath = bookmarksList[siteIndex].siteUrl;
+  window.open(sitePath, "_blank");
+}
+
+// ? Deleting site from the bookmarks list
+function deleteSiteBtn(siteIndex) {
+  bookmarksList.splice(siteIndex, 1);
+  displayBookmarks();
+  localStorage.setItem("bookmarksList", JSON.stringify(bookmarksList));
+}
+
+// ? Taking values from the bookmarks list to edit the bookmark
+function editSiteBtn(siteIndex) {
+  currentSiteIndex = siteIndex;
+  siteTitleInput.value = bookmarksList[siteIndex].siteTitle;
+  siteUrlInput.value = bookmarksList[siteIndex].siteUrl;
+  siteDescriptionInput.value = bookmarksList[siteIndex].siteDesc;
+
+  updateSiteBtn.classList.remove("d-none");
+  addSiteBtn.classList.add("d-none");
+}
+
+// ? Sending values back to the bookmarks list to make updates to the bookmark.
+updateSiteBtn.addEventListener("click", function () {
+  let updatedSite = {
+    siteTitle: siteTitleInput.value,
+    siteUrl: siteUrlInput.value,
+    siteDesc: siteDescriptionInput.value
+  };
+
+  bookmarksList[currentSiteIndex] = updatedSite;
+  displayBookmarks();
+  clearInputs();
+  localStorage.setItem("bookmarksList", JSON.stringify(bookmarksList));
+  updateSiteBtn.classList.add("d-none");
+  addSiteBtn.classList.remove("d-none");
 });
